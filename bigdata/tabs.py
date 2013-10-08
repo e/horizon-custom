6696adbe9115d2ccbd48d758633a1848a0b3e252
marcos <m@rcosbartolo.me>
2013-10-08 12:48:46 +0200

    Better detail tabs

--- bigdata/tabs.py
+++ bigdata/tabs.py
@@ -3,19 +3,25 @@ from django.utils.translation import ugettext_lazy as _
 from horizon import exceptions, tabs
 
 
-class DetailsTab(tabs.Tab):
-    name = _("Details")
-    slug="details"
-    template_name = ("project/bigdata/_bigdata_details.html")
+class OverviewTab(tabs.Tab):
+    name = _("Overview")
+    slug="overview"
+    template_name = ("project/bigdata/_detail_overview.html")
 
     def get_context_data(self, request):
-        return {"bigdata": self.tab_group.kwargs['instance_id']}
+        data = self.tab_group.kwargs['bigdata']['config']
+        return {"bigdata":     data['id'],
+                "api_secret":  data['api_secret'],
+                "ext_data":    data['extdata'],
+                "ssk_pub_key": data['ssk_pub_key'],
+                "api_key":     data['api_key'],
+                "url":         data['url'],}
 
 
-class AnotherTab(tabs.Tab):
-    name = _("Additional Data")
-    slug="additional_data"
-    template_name = ("project/bigdata/_additional_data.html")
+class LogTab(tabs.Tab):
+    name = _("Log")
+    slug="log"
+    template_name = ("project/bigdata/_detail_log.html")
 
     def get_context_data(self, request):
         return {"bigdata": self.tab_group.kwargs['instance_id']}
@@ -24,5 +30,5 @@ class AnotherTab(tabs.Tab):
 
 class BigdataTabs(tabs.TabGroup):
     slug = "bigdata_details"
-    tabs = (DetailsTab, AnotherTab)
+    tabs = (OverviewTab, LogTab)
     sticky = True
